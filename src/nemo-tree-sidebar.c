987a61844fc567f451bc0372f36beffe19b988d2
Michael Webster <miketwebster@gmail.com>
2014-02-16 18:29:22 -0500

    hidpi support for tree sidebar

--- src/nemo-tree-sidebar.c
+++ src/nemo-tree-sidebar.c
@@ -1407,6 +1407,13 @@ create_popup_menu (FMTreeView *view)
     add_action_popup_items (view);
 }
 
+static gint
+get_icon_scale_callback (FMTreeModel *model,
+                         FMTreeView  *view)
+{
+   return gtk_widget_get_scale_factor (GTK_WIDGET (view->details->tree_widget));
+}
+
 static void
 create_tree (FMTreeView *view)
 {
@@ -1432,10 +1439,12 @@ create_tree (FMTreeView *view)
 	gtk_tree_sortable_set_default_sort_func (GTK_TREE_SORTABLE (view->details->sort_model),
 						 compare_rows, view, NULL);
 
-	g_signal_connect_object
-		(view->details->child_model, "row_loaded",
-		 G_CALLBACK (row_loaded_callback),
-		 view, G_CONNECT_AFTER);
+	g_signal_connect_object (view->details->child_model, "row_loaded",
+		                     G_CALLBACK (row_loaded_callback),
+                             view, G_CONNECT_AFTER);
+    g_signal_connect_object (view->details->child_model, "get-icon-scale",
+                             G_CALLBACK (get_icon_scale_callback), view, 0);
+
 #ifdef NOT_YET_USABLE /* Do we really want this? */
 	icon = g_themed_icon_new (NEMO_ICON_COMPUTER);
 	fm_tree_model_add_root_uri (view->details->child_model, "computer:///", _("Computer"), icon, NULL);
@@ -1498,9 +1507,9 @@ create_tree (FMTreeView *view)
 	cell = gtk_cell_renderer_pixbuf_new ();
 	gtk_tree_view_column_pack_start (column, cell, FALSE);
 	gtk_tree_view_column_set_attributes (column, cell,
-					     "pixbuf", FM_TREE_MODEL_CLOSED_PIXBUF_COLUMN,
-					     "pixbuf_expander_closed", FM_TREE_MODEL_CLOSED_PIXBUF_COLUMN,
-					     "pixbuf_expander_open", FM_TREE_MODEL_OPEN_PIXBUF_COLUMN,
+					     "surface", FM_TREE_MODEL_CLOSED_SURFACE_COLUMN,
+					   //  "pixbuf_expander_closed", FM_TREE_MODEL_CLOSED_SURFACE_COLUMN,
+					   //  "pixbuf_expander_open", FM_TREE_MODEL_OPEN_SURFACE_COLUMN,
 					     NULL);
 	
 	cell = gtk_cell_renderer_text_new ();
